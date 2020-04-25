d200c9bdd7c577727f836ca8c22181f9d62d4445
Alex Bonillo <alex.bonillo.externo@axa-assistance.es>
2020-04-25 17:21:17 +0200

    new post page created

---
+++ src/pages/new-post/new-post.styled.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const NewPostContainer = styled.div`
+  width: 50%;
+  margin: 30px auto 50px;
+  border: 1px solid #999;
+  border-radius: 5px;
+  background-color: #ddd;
+  padding: 30px;
+  min-height: 500px;
+
+  @media (max-width: 768px) and (min-width: 601px) {
+    width: 80%;
+    padding: 15px;
+  }
+
+  @media (max-width: 600px) {
+    width: 95%;
+    padding: 15px;
+  }
+`;
+
+export const PostNotFound = styled.div`
+  text-align: center;
+  font-weight: bold;
+  margin-top: 70px;
+`;
+
+export const BackLinkContainer = styled.div`
+  width: 50%;
+  margin-bottom: 10px;
+  margin: 0 auto;
+
+  @media (max-width: 768px) and (min-width: 601px) {
+    width: 80%;
+  }
+
+  @media (max-width: 600px) {
+    width: 95%;
+  }
+`;
--- src/pages/new-post/new-post.tsx
+++ src/pages/new-post/new-post.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import { MainContainer } from 'components/container';
+import { MainButtonLink } from 'components/button';
+import { BackLinkContainer, NewPostContainer } from './new-post.styled';
+import { useDispatch } from 'react-redux';
+import { addPost } from 'redux/posts';
+import { Post } from 'redux/types';
+
 const NewPost: React.FC<{}> = () => {
+  const dispatch = useDispatch();
+  const onPostCreate = (postData: Post) => {
+    dispatch(addPost(postData));
+  };
+
   return (
-    <h1>NEW POST PAGE</h1>
-  )
+    <MainContainer>
+      <h1>Create your own post!</h1>
+      <BackLinkContainer>
+        <MainButtonLink to='/'>Go Back to List</MainButtonLink>
+      </BackLinkContainer>
+      <NewPostContainer data-testid='new-post__form-container'>
+        <h3>HERE THERE WILL BE A FORM</h3>
+      </NewPostContainer>
+    </MainContainer>
+  );
 };
 
 export default NewPost;
